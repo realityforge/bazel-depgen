8f6148296ecc33361f18ca7376228a7f84973a7a
Peter Donald <peter@realityforge.org>
2019-03-24 23:04:06 +1100

    Add exception that can be thrown that indicates exit code

---
+++ src/main/java/org/realityforge/bazel/depgen/TerminalStateException.java
@@ -0,0 +1,25 @@
+package org.realityforge.bazel.depgen;
+
+import javax.annotation.Nullable;
+
+public class TerminalStateException
+  extends RuntimeException
+{
+  private final int _exitCode;
+
+  public TerminalStateException( @Nullable final String message, final int exitCode )
+  {
+    this( message, null, exitCode );
+  }
+
+  public TerminalStateException( @Nullable final String message, @Nullable final Throwable cause, final int exitCode )
+  {
+    super( message, cause );
+    _exitCode = exitCode;
+  }
+
+  public int getExitCode()
+  {
+    return _exitCode;
+  }
+}
