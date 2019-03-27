f0dbbb7e4d0f23898b462ee196ced364aa8dcfc2
Peter Donald <peter@realityforge.org>
2019-03-27 21:41:11 +1100

    Add model for options

---
+++ src/main/java/org/realityforge/bazel/depgen/model/OptionsModel.java
@@ -0,0 +1,40 @@
+package org.realityforge.bazel.depgen.model;
+
+import java.util.Objects;
+import javax.annotation.Nonnull;
+import org.realityforge.bazel.depgen.config.OptionsConfig;
+
+public final class OptionsModel
+{
+  @Nonnull
+  private final OptionsConfig _source;
+
+  @Nonnull
+  public static OptionsModel parse( @Nonnull final OptionsConfig source )
+  {
+    return new OptionsModel( source );
+  }
+
+  private OptionsModel( @Nonnull final OptionsConfig source )
+  {
+    _source = Objects.requireNonNull( source );
+  }
+
+  @Nonnull
+  public OptionsConfig getSource()
+  {
+    return _source;
+  }
+
+  @Nonnull
+  public String getWorkspaceDirectory()
+  {
+    return _source.getWorkspaceDirectory();
+  }
+
+  @Nonnull
+  public String getExtensionFile()
+  {
+    return _source.getExtensionFile();
+  }
+}
---
+++ src/test/java/org/realityforge/bazel/depgen/model/OptionsModelTest.java
@@ -0,0 +1,34 @@
+package org.realityforge.bazel.depgen.model;
+
+import org.realityforge.bazel.depgen.AbstractTest;
+import org.realityforge.bazel.depgen.config.OptionsConfig;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+public class OptionsModelTest
+  extends AbstractTest
+{
+  @Test
+  public void parseWithDefaults()
+  {
+    final OptionsConfig source = new OptionsConfig();
+
+    final OptionsModel model = OptionsModel.parse( source );
+    assertEquals( model.getSource(), source );
+    assertEquals( model.getWorkspaceDirectory(), OptionsConfig.DEFAULT_WORKSPACE_DIR );
+    assertEquals( model.getExtensionFile(), OptionsConfig.DEFAULT_EXTENSION_FILE );
+  }
+
+  @Test
+  public void parse()
+  {
+    final OptionsConfig source = new OptionsConfig();
+    source.setWorkspaceDirectory( ".." );
+    source.setExtensionFile( "workspace.bzl" );
+
+    final OptionsModel model = OptionsModel.parse( source );
+    assertEquals( model.getSource(), source );
+    assertEquals( model.getWorkspaceDirectory(), ".." );
+    assertEquals( model.getExtensionFile(), "workspace.bzl" );
+  }
+}
