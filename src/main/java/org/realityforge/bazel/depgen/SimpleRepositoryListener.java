8733a4040503ca007e46d913377e0ab7200c20ed
Peter Donald <peter@realityforge.org>
2019-03-28 17:28:18 +1100

    Set logging levels as appropriate

--- src/main/java/org/realityforge/bazel/depgen/SimpleRepositoryListener.java
+++ src/main/java/org/realityforge/bazel/depgen/SimpleRepositoryListener.java
@@ -20,102 +20,102 @@ final class SimpleRepositoryListener
   @Override
   public void artifactDeployed( @Nonnull final RepositoryEvent event )
   {
-    _logger.info( "Deployed " + event.getArtifact() + " to " + event.getRepository() );
+    _logger.fine( "Deployed " + event.getArtifact() + " to " + event.getRepository() );
   }
 
   @Override
   public void artifactDeploying( @Nonnull final RepositoryEvent event )
   {
-    _logger.info( "Deploying " + event.getArtifact() + " to " + event.getRepository() );
+    _logger.fine( "Deploying " + event.getArtifact() + " to " + event.getRepository() );
   }
 
   @Override
   public void artifactDescriptorInvalid( @Nonnull final RepositoryEvent event )
   {
-    _logger.info( "Invalid artifact descriptor for " + event.getArtifact() + ": " + event.getException().getMessage() );
+    _logger.warning( "Invalid artifact descriptor for " + event.getArtifact() + ": " + event.getException().getMessage() );
   }
 
   @Override
   public void artifactDescriptorMissing( @Nonnull final RepositoryEvent event )
   {
-    _logger.info( "Missing artifact descriptor for " + event.getArtifact() );
+    _logger.warning( "Missing artifact descriptor for " + event.getArtifact() );
   }
 
   @Override
   public void artifactInstalled( @Nonnull final RepositoryEvent event )
   {
-    _logger.info( "Installed " + event.getArtifact() + " to " + event.getFile() );
+    _logger.fine( "Installed " + event.getArtifact() + " to " + event.getFile() );
   }
 
   @Override
   public void artifactInstalling( @Nonnull final RepositoryEvent event )
   {
-    _logger.info( "Installing " + event.getArtifact() + " to " + event.getFile() );
+    _logger.fine( "Installing " + event.getArtifact() + " to " + event.getFile() );
   }
 
   @Override
   public void artifactResolved( @Nonnull final RepositoryEvent event )
   {
-    _logger.info( "Resolved artifact " + event.getArtifact() + " from " + event.getRepository() );
+    _logger.fine( "Resolved artifact " + event.getArtifact() + " from " + event.getRepository() );
   }
 
   @Override
   public void artifactDownloading( @Nonnull final RepositoryEvent event )
   {
-    _logger.info( "Downloading artifact " + event.getArtifact() + " from " + event.getRepository() );
+    _logger.fine( "Downloading artifact " + event.getArtifact() + " from " + event.getRepository() );
   }
 
   @Override
   public void artifactDownloaded( @Nonnull final RepositoryEvent event )
   {
-    _logger.info( "Downloaded artifact " + event.getArtifact() + " from " + event.getRepository() );
+    _logger.fine( "Downloaded artifact " + event.getArtifact() + " from " + event.getRepository() );
   }
 
   @Override
   public void artifactResolving( @Nonnull final RepositoryEvent event )
   {
-    _logger.info( "Resolving artifact " + event.getArtifact() );
+    _logger.fine( "Resolving artifact " + event.getArtifact() );
   }
 
   @Override
   public void metadataDeployed( @Nonnull final RepositoryEvent event )
   {
-    _logger.info( "Deployed " + event.getMetadata() + " to " + event.getRepository() );
+    _logger.fine( "Deployed " + event.getMetadata() + " to " + event.getRepository() );
   }
 
   @Override
   public void metadataDeploying( @Nonnull final RepositoryEvent event )
   {
-    _logger.info( "Deploying " + event.getMetadata() + " to " + event.getRepository() );
+    _logger.fine( "Deploying " + event.getMetadata() + " to " + event.getRepository() );
   }
 
   @Override
   public void metadataInstalled( @Nonnull final RepositoryEvent event )
   {
-    _logger.info( "Installed " + event.getMetadata() + " to " + event.getFile() );
+    _logger.fine( "Installed " + event.getMetadata() + " to " + event.getFile() );
   }
 
   @Override
   public void metadataInstalling( @Nonnull final RepositoryEvent event )
   {
-    _logger.info( "Installing " + event.getMetadata() + " to " + event.getFile() );
+    _logger.fine( "Installing " + event.getMetadata() + " to " + event.getFile() );
   }
 
   @Override
   public void metadataInvalid( @Nonnull final RepositoryEvent event )
   {
-    _logger.info( "Invalid metadata " + event.getMetadata() );
+    _logger.warning( "Invalid metadata " + event.getMetadata() );
   }
 
   @Override
   public void metadataResolved( @Nonnull final RepositoryEvent event )
   {
-    _logger.info( "Resolved metadata " + event.getMetadata() + " from " + event.getRepository() );
+    _logger.fine( "Resolved metadata " + event.getMetadata() + " from " + event.getRepository() );
   }
 
   @Override
   public void metadataResolving( @Nonnull final RepositoryEvent event )
   {
-    _logger.info( "Resolving metadata " + event.getMetadata() + " from " + event.getRepository() );
+    _logger.fine( "Resolving metadata " + event.getMetadata() + " from " + event.getRepository() );
   }
 }
