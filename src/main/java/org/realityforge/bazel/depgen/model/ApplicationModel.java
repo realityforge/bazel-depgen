4abcbe5d7c7fe6b046fce8bd33466fe4ef4cdec4
Peter Donald <peter@realityforge.org>
2019-03-30 14:14:00 +1100

    Use order consistent with underlying config

--- src/main/java/org/realityforge/bazel/depgen/model/ApplicationModel.java
+++ src/main/java/org/realityforge/bazel/depgen/model/ApplicationModel.java
@@ -13,37 +13,44 @@ public final class ApplicationModel
   @Nonnull
   private final ApplicationConfig _source;
   @Nonnull
+  private final OptionsModel _options;
+  @Nonnull
   private final List<ArtifactModel> _artifacts;
   @Nonnull
   private final List<ReplacementModel> _replacements;
-  @Nonnull
-  private final OptionsModel _options;
 
   @Nonnull
   public static ApplicationModel parse( @Nonnull final ApplicationConfig source )
   {
+    final OptionsModel optionsModel = OptionsModel.parse( source.getOptions() );
     final List<ArtifactModel> artifactModels =
       source.getArtifacts().stream().flatMap( c -> ArtifactModel.parse( c ).stream() ).collect( Collectors.toList() );
     final List<ReplacementModel> replacements =
       source.getReplacements().stream().map( ReplacementModel::parse ).collect( Collectors.toList() );
-    return new ApplicationModel( source, artifactModels, replacements, OptionsModel.parse( source.getOptions() ) );
+    return new ApplicationModel( source, optionsModel, artifactModels, replacements );
   }
 
   private ApplicationModel( @Nonnull final ApplicationConfig source,
+                            @Nonnull final OptionsModel options,
                             @Nonnull final List<ArtifactModel> artifacts,
-                            @Nonnull final List<ReplacementModel> replacements,
-                            @Nonnull final OptionsModel options )
+                            @Nonnull final List<ReplacementModel> replacements )
   {
     _source = Objects.requireNonNull( source );
+    _options = Objects.requireNonNull( options );
     _artifacts = Objects.requireNonNull( artifacts );
     _replacements = Objects.requireNonNull( replacements );
-    _options = Objects.requireNonNull( options );
   }
 
   @Nonnull
   public ApplicationConfig getSource()
   {
     return _source;
+  }
+
+  @Nonnull
+  public OptionsModel getOptions()
+  {
+    return _options;
   }
 
   @Nonnull
@@ -62,11 +69,5 @@ public final class ApplicationModel
   public List<ReplacementModel> getReplacements()
   {
     return _replacements;
-  }
-
-  @Nonnull
-  public OptionsModel getOptions()
-  {
-    return _options;
   }
 }
