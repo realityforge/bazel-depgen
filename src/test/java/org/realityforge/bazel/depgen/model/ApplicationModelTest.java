29ad23cb7e640eedcb1a4de77eb0ed6c9f9d0d2e
Peter Donald <peter@realityforge.org>
2019-03-30 14:17:31 +1100

    Enhance test to show replacements being added

--- src/test/java/org/realityforge/bazel/depgen/model/ApplicationModelTest.java
+++ src/test/java/org/realityforge/bazel/depgen/model/ApplicationModelTest.java
@@ -1,10 +1,12 @@
 package org.realityforge.bazel.depgen.model;
 
 import java.util.Collections;
+import java.util.List;
 import org.realityforge.bazel.depgen.AbstractTest;
 import org.realityforge.bazel.depgen.config.ApplicationConfig;
 import org.realityforge.bazel.depgen.config.ArtifactConfig;
 import org.realityforge.bazel.depgen.config.OptionsConfig;
+import org.realityforge.bazel.depgen.config.ReplacementConfig;
 import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
@@ -19,6 +21,11 @@ public class ApplicationModelTest
     artifactConfig.setCoord( "com.example:myapp:1.0" );
     source.setArtifacts( Collections.singletonList( artifactConfig ) );
 
+    final ReplacementConfig replacement = new ReplacementConfig();
+    replacement.setCoord( "com.example:alib" );
+    replacement.setTarget( "@com_example//:alib" );
+    source.setReplacements( Collections.singletonList( replacement ) );
+
     final ApplicationModel model = ApplicationModel.parse( source );
     assertEquals( model.getSource(), source );
     assertEquals( model.getRepositories().get( ApplicationConfig.MAVEN_CENTRAL_ID ),
@@ -30,6 +37,12 @@ public class ApplicationModelTest
     assertEquals( artifactModel.getGroup(), "com.example" );
     assertEquals( artifactModel.getId(), "myapp" );
     assertEquals( artifactModel.getVersion(), "1.0" );
-    assertTrue( model.getReplacements().isEmpty() );
+
+    final List<ReplacementModel> replacements = model.getReplacements();
+    assertEquals( replacements.size(), 1 );
+    final ReplacementModel replacementModel = replacements.get( 0 );
+    assertEquals( replacementModel.getGroup(), "com.example" );
+    assertEquals( replacementModel.getId(), "alib" );
+    assertEquals( replacementModel.getTarget(), "@com_example//:alib" );
   }
 }
