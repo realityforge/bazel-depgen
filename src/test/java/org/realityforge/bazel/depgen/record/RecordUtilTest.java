b0a5d09a0f3603007f57261912f83b1c7429d3ec
Peter Donald <peter@realityforge.org>
2019-04-30 20:14:15 +1000

    Add test for RecordUtil.toArtifactKey

--- src/test/java/org/realityforge/bazel/depgen/record/RecordUtilTest.java
+++ src/test/java/org/realityforge/bazel/depgen/record/RecordUtilTest.java
@@ -1,11 +1,19 @@
 package org.realityforge.bazel.depgen.record;
 
 import org.eclipse.aether.artifact.DefaultArtifact;
+import org.eclipse.aether.graph.DefaultDependencyNode;
 import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
 public class RecordUtilTest
 {
+  @Test
+  public void toArtifactKey_dependencyNode()
+  {
+    assertEquals( RecordUtil.toArtifactKey( new DefaultDependencyNode( new DefaultArtifact( "com.example:mylib:jar:0.98" ) ) ),
+                  "com.example:mylib" );
+  }
+
   @Test
   public void artifactToPath()
   {
