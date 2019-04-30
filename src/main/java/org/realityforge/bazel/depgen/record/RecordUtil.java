52126ee12ccd10201d301fb680c036ad2ade1adf
Peter Donald <peter@realityforge.org>
2019-04-30 20:53:05 +1000

    Add utility to derive urls

--- src/main/java/org/realityforge/bazel/depgen/record/RecordUtil.java
+++ src/main/java/org/realityforge/bazel/depgen/record/RecordUtil.java
@@ -2,10 +2,17 @@ package org.realityforge.bazel.depgen.record;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 import javax.annotation.Nonnull;
 import org.eclipse.aether.artifact.Artifact;
 import org.eclipse.aether.graph.DependencyNode;
+import org.eclipse.aether.repository.RemoteRepository;
 import org.realityforge.bazel.depgen.model.ArtifactModel;
 import org.realityforge.bazel.depgen.util.HashUtil;
 
@@ -57,5 +64,52 @@ final class RecordUtil
     {
       throw new IllegalStateException( "Error generating sha256 hash for file " + file, ioe );
     }
+  }
+
+  @Nonnull
+  static List<String> deriveUrls( @Nonnull final Artifact artifact, @Nonnull final List<RemoteRepository> repositories )
+  {
+    final ArrayList<String> urls = new ArrayList<>();
+    for ( final RemoteRepository remoteRepository : repositories )
+    {
+      try
+      {
+        final String repoUrl = remoteRepository.getUrl();
+        final URI uri = new URI( repoUrl + (repoUrl.endsWith( "/" ) ? "" : "/") + artifactToPath( artifact ) );
+        final URL url = uri.toURL();
+        final String protocol = url.getProtocol();
+        if ( "http".equals( protocol ) || "https".equals( protocol ) )
+        {
+          final HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+          connection.setRequestMethod( "HEAD" );
+          connection.connect();
+          final int responseCode = connection.getResponseCode();
+          if ( 200 == responseCode )
+          {
+            urls.add( url.toExternalForm() );
+          }
+        }
+        else if ( "file".equals( protocol ) )
+        {
+          // Attempt to open file and if it is present then there should be no exception
+          url.openStream().close();
+          urls.add( uri.toString() );
+        }
+        else
+        {
+          final String message = "Unsupported repository protocol for " + artifact + " with url " + url + ".";
+          throw new IllegalStateException( message );
+        }
+      }
+      catch ( final IOException | URISyntaxException ignored )
+      {
+      }
+    }
+
+    if ( urls.isEmpty() )
+    {
+      throw new IllegalStateException( "Unable to locate artifact " + artifact + " in any repository." );
+    }
+    return urls;
   }
 }
--- src/test/java/org/realityforge/bazel/depgen/record/RecordUtilTest.java
+++ src/test/java/org/realityforge/bazel/depgen/record/RecordUtilTest.java
@@ -1,9 +1,13 @@
 package org.realityforge.bazel.depgen.record;
 
+import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
 import org.eclipse.aether.artifact.DefaultArtifact;
 import org.eclipse.aether.graph.DefaultDependencyNode;
+import org.eclipse.aether.repository.RemoteRepository;
 import org.realityforge.bazel.depgen.AbstractTest;
 import org.realityforge.bazel.depgen.FileUtil2;
 import org.testng.annotations.Test;
@@ -49,6 +53,35 @@ public class RecordUtilTest
       final IllegalStateException exception =
         expectThrows( IllegalStateException.class, () -> RecordUtil.sha256( filename.toFile() ) );
       assertEquals( exception.getMessage(), "Error generating sha256 hash for file " + filename.toFile() );
+    } );
+  }
+
+  @Test
+  public void deriveUrls_multipleFileURLs()
+    throws Exception
+  {
+    inIsolatedDirectory( () -> {
+      final Path dir1 = FileUtil2.createLocalTempDir();
+      final Path dir2 = FileUtil2.createLocalTempDir();
+      final Path dir3 = FileUtil2.createLocalTempDir();
+
+      final URI uri = dir1.toUri();
+
+      final RemoteRepository repo1 = new RemoteRepository.Builder( "dir1", "default", uri.toString() ).build();
+      final RemoteRepository repo2 = new RemoteRepository.Builder( "dir2", "default", dir2.toUri().toString() ).build();
+      final RemoteRepository repo3 = new RemoteRepository.Builder( "dir3", "default", dir3.toUri().toString() ).build();
+
+      deployTempArtifactToLocalRepository( dir1, "com.example:myapp:1.0" );
+      deployTempArtifactToLocalRepository( dir2, "com.example:myapp:1.0" );
+
+      final List<String> urls =
+        RecordUtil.deriveUrls( new DefaultArtifact( "com.example:myapp:jar:1.0" ),
+                               Arrays.asList( repo1, repo2, repo3 ) );
+      assertEquals( urls.size(), 2 );
+      assertTrue( urls.get( 0 ).startsWith( repo1.getUrl() ) );
+      assertTrue( urls.get( 1 ).startsWith( repo2.getUrl() ) );
+      assertTrue( urls.get( 0 ).endsWith( "com/example/myapp/1.0/myapp-1.0.jar" ) );
+      assertTrue( urls.get( 1 ).endsWith( "com/example/myapp/1.0/myapp-1.0.jar" ) );
     } );
   }
 }
