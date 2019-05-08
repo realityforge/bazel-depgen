6a42ab6cc8d5b08fcd45678d92de3e8699f3c6a2
Peter Donald <peter@realityforge.org>
2019-05-08 22:45:44 +1000

    Fix assertion

--- src/test/java/org/realityforge/bazel/depgen/record/ApplicationRecordTest.java
+++ src/test/java/org/realityforge/bazel/depgen/record/ApplicationRecordTest.java
@@ -437,7 +437,7 @@ public class ApplicationRecordTest
         assertNotNull( artifactRecord.getArtifactModel() );
         assertEquals( artifactRecord.getKey(), "com.example:myapp" );
         assertEquals( artifactRecord.getName(), "com_example__myapp__1_0" );
-        assertEquals( artifactRecord.getAlias(), "com_example__myapp_" );
+        assertEquals( artifactRecord.getAlias(), "com_example__myapp" );
         assertEquals( artifactRecord.getSha256(),
                       "E424B659CF9C9C4ADF4C19A1CACDB13C0CBD78A79070817F433DBC2DADE3C6D4" );
         assertEquals( artifactRecord.getUrls(),
