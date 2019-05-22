cb5e00dfd2335c5881f7f2c2f93c4f0e2b9b3dcd
Peter Donald <peter@realityforge.org>
2019-05-23 08:26:37 +1000

    Start to record target suffix

--- src/main/java/org/realityforge/bazel/depgen/config/Language.java
+++ src/main/java/org/realityforge/bazel/depgen/config/Language.java
@@ -1,7 +1,23 @@
 package org.realityforge.bazel.depgen.config;
 
+import java.util.Objects;
+import javax.annotation.Nonnull;
+
 public enum Language
 {
-  Java,
-  J2cl
+  Java( "-java" ),
+  J2cl( "-j2cl" );
+  @Nonnull
+  private final String _targetSuffix;
+
+  Language( @Nonnull final String targetSuffix )
+  {
+    _targetSuffix = Objects.requireNonNull( targetSuffix );
+  }
+
+  @Nonnull
+  public String getTargetSuffix()
+  {
+    return _targetSuffix;
+  }
 }
