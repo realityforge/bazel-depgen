76c64b24c7344b6c16c4be66ddced949fe044160
Peter Donald <peter@realityforge.org>
2019-05-26 13:44:25 +1000

    Import a representation of licenses according to Bazel

---
+++ src/main/java/org/realityforge/bazel/depgen/config/License.java
@@ -0,0 +1,32 @@
+package org.realityforge.bazel.depgen.config;
+
+/**
+ * Types of licenses. Taken from https://docs.bazel.build/versions/master/be/functions.html#licenses
+ */
+public enum License
+{
+  /**
+   * Requires mandatory source distribution.
+   */
+  restricted,
+
+  /**
+   * Allows usage of software freely in unmodified form. Any modifications must be made freely available.
+   */
+  reciprocal,
+
+  /**
+   * Original or modified third-party software may be shipped without danger nor encumbering other sources. All of the licenses in this category do, however, have an "original Copyright notice" or "advertising clause", wherein any external distributions must include the notice or clause specified in the license.
+   */
+  notice,
+
+  /**
+   * Code that is under a license but does not require a notice.
+   */
+  permissive,
+
+  /**
+   * Public domain, free for any use.
+   */
+  unencumbered
+}
