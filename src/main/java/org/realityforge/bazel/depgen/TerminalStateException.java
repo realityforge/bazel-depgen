285c02522f7fcd4142a6446711efc4d88e944612
Peter Donald <peter@realityforge.org>
2019-05-28 19:42:57 +1000

    Reduce access levels

--- src/main/java/org/realityforge/bazel/depgen/TerminalStateException.java
+++ src/main/java/org/realityforge/bazel/depgen/TerminalStateException.java
@@ -2,28 +2,28 @@ package org.realityforge.bazel.depgen;
 
 import javax.annotation.Nullable;
 
-public class TerminalStateException
+class TerminalStateException
   extends RuntimeException
 {
   private final int _exitCode;
 
-  public TerminalStateException( final int exitCode )
+  TerminalStateException( final int exitCode )
   {
     this( null, exitCode );
   }
 
-  public TerminalStateException( @Nullable final String message, final int exitCode )
+  TerminalStateException( @Nullable final String message, final int exitCode )
   {
     this( message, null, exitCode );
   }
 
-  public TerminalStateException( @Nullable final String message, @Nullable final Throwable cause, final int exitCode )
+  TerminalStateException( @Nullable final String message, @Nullable final Throwable cause, final int exitCode )
   {
     super( message, cause );
     _exitCode = exitCode;
   }
 
-  public int getExitCode()
+  int getExitCode()
   {
     return _exitCode;
   }
