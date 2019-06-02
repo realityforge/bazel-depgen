7cf4dc2b10f77d4fa97a5f94e4e1ffa2fe123704
Peter Donald <peter@realityforge.org>
2019-06-02 15:19:08 +1000

    Add utilities to retrieve repository cache setting from bazel

--- src/main/java/org/realityforge/bazel/depgen/util/BazelUtil.java
+++ src/main/java/org/realityforge/bazel/depgen/util/BazelUtil.java
@@ -1,6 +1,10 @@
 package org.realityforge.bazel.depgen.util;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.util.logging.Logger;
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 public final class BazelUtil
 {
@@ -12,5 +16,45 @@ public final class BazelUtil
   public static String cleanNamePart( @Nonnull final String name )
   {
     return name.toLowerCase().replaceAll( "[^a-z0-9]", "_" );
+  }
+
+  @Nullable
+  public static File getRepositoryCache( @Nonnull final Logger logger, @Nonnull final File cwd )
+  {
+    try
+    {
+      final String repositoryCache =
+        Exec.capture( p -> p.command( "bazel", "info", "repository_cache" ).directory( cwd ), 0 );
+      return new File( repositoryCache.trim() );
+    }
+    catch ( final Exception e )
+    {
+      logger.info( "WARNING: Unable to locate bazel repository cache. Using default bazel repository cache." );
+      return getDefaultRepositoryCache( logger );
+    }
+  }
+
+  @SuppressWarnings( "ResultOfMethodCallIgnored" )
+  @Nullable
+  static File getDefaultRepositoryCache( @Nonnull final Logger logger )
+  {
+    try
+    {
+      final File dir = File.createTempFile( "bazel-depgen", "dir" );
+      dir.delete();
+      dir.mkdir();
+      final File file = new File( dir, "WORKSPACE" );
+      Files.write( file.toPath(), new byte[ 0 ] );
+      final String repositoryCache =
+        Exec.capture( p -> p.command( "bazel", "info", "repository_cache" ).directory( dir ), 0 );
+      file.delete();
+      dir.delete();
+      return new File( repositoryCache.trim() );
+    }
+    catch ( final Throwable ignored )
+    {
+      logger.severe( "Error: Failed to determine default bazel repository cache." );
+      return null;
+    }
   }
 }
--- src/test/java/org/realityforge/bazel/depgen/util/BazelUtilTest.java
+++ src/test/java/org/realityforge/bazel/depgen/util/BazelUtilTest.java
@@ -1,6 +1,12 @@
 package org.realityforge.bazel.depgen.util;
 
+import gir.io.FileUtil;
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import org.realityforge.bazel.depgen.AbstractTest;
+import org.realityforge.bazel.depgen.TestHandler;
 import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
@@ -12,5 +18,58 @@ public class BazelUtilTest
   {
     assertEquals( BazelUtil.cleanNamePart( "com.example:mylib:0.98" ), "com_example_mylib_0_98" );
     assertEquals( BazelUtil.cleanNamePart( "com.example:My-App:22-RC1" ), "com_example_my_app_22_rc1" );
+  }
+
+  @Test
+  public void getDefaultRepositoryCache()
+    throws Exception
+  {
+    // These tests assume that there is no WORKSPACE in parent directory from isolated directory
+    // They also assume that bazel is present on build machine
+    inIsolatedDirectory( () -> {
+      final TestHandler handler = new TestHandler();
+      final File repositoryCache = BazelUtil.getDefaultRepositoryCache( createLogger( handler ) );
+      assertNotNull( repositoryCache );
+      assertTrue( repositoryCache.getAbsolutePath().endsWith( "/cache/repos/v1" ) );
+      assertEquals( handler.toString(), "" );
+    } );
+  }
+
+  @Test
+  public void getRepositoryCache()
+    throws Exception
+  {
+    // These tests assume that there is no WORKSPACE in parent directory from isolated directory
+    // They also assume that bazel is present on build machine
+    inIsolatedDirectory( () -> {
+      final Path cwd = FileUtil.getCurrentDirectory();
+      final Path dir = FileUtil.createLocalTempDir();
+      Files.write( cwd.resolve( "WORKSPACE" ), new byte[ 0 ] );
+      Files.write( cwd.resolve( ".bazelrc" ),
+                   ( "build --repository_cache " + dir ).getBytes( StandardCharsets.US_ASCII ) );
+      final TestHandler handler = new TestHandler();
+      final File repositoryCache = BazelUtil.getRepositoryCache( createLogger( handler ), cwd
+        .toFile() );
+      assertEquals( handler.toString(), "" );
+      assertNotNull( repositoryCache );
+      assertEquals( repositoryCache.toPath().toAbsolutePath().normalize(), dir );
+    } );
+  }
+
+  @Test
+  public void getRepositoryCache_WROKSPACE_notPresent()
+    throws Exception
+  {
+    // These tests assume that there is no WORKSPACE in parent directory from isolated directory
+    // They also assume that bazel is present on build machine
+    inIsolatedDirectory( () -> {
+      final TestHandler handler = new TestHandler();
+      final File repositoryCache =
+        BazelUtil.getRepositoryCache( createLogger( handler ), FileUtil.getCurrentDirectory().toFile() );
+      assertEquals( handler.toString(),
+                    "WARNING: Unable to locate bazel repository cache. Using default bazel repository cache." );
+      assertNotNull( repositoryCache );
+      assertTrue( repositoryCache.getAbsolutePath().endsWith( "/cache/repos/v1" ) );
+    } );
   }
 }
