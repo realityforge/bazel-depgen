cbca8939ad11d2e5fbe09cf40381bd90d0be5627
Peter Donald <peter@realityforge.org>
2019-06-02 15:17:28 +1000

    Add a simple utility for executing external commands

---
+++ src/main/java/org/realityforge/bazel/depgen/util/Exec.java
@@ -0,0 +1,99 @@
+package org.realityforge.bazel.depgen.util;
+
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.function.Consumer;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+/**
+ * Utility methods for invoking native commands.
+ */
+final class Exec
+{
+  private static final int BUFFER_SIZE = 2 * 1024;
+
+  private Exec()
+  {
+  }
+
+  /**
+   * Execute a command and capture the output.
+   *
+   * @param action the callback responsible for setting up ProcessBuilder.
+   * @return the output of the command.
+   */
+  @SuppressWarnings( "SameParameterValue" )
+  @Nonnull
+  static String capture( @Nonnull final Consumer<ProcessBuilder> action, @Nullable final Integer expectedExitCode )
+  {
+    final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    exec( action, process -> copy( process.getInputStream(), new BufferedOutputStream( baos ) ), expectedExitCode );
+    return baos.toString();
+  }
+
+  /**
+   * Low level utility for executing a process.
+   * This method will return when the process completes.
+   *
+   * @param action           the callback responsible for setting up ProcessBuilder.
+   * @param processHandler   the callback passed a process.
+   * @param expectedExitCode the expected exitCode of the process.
+   * @return the exitCode.
+   */
+  static int exec( @Nonnull final Consumer<ProcessBuilder> action,
+                   @Nullable final Consumer<Process> processHandler,
+                   @Nullable final Integer expectedExitCode )
+  {
+    final ProcessBuilder builder = new ProcessBuilder();
+    action.accept( builder );
+    try
+    {
+      final Process process = builder.start();
+      if ( null != processHandler )
+      {
+        processHandler.accept( process );
+      }
+      final int exitCode = process.waitFor();
+      if ( null != expectedExitCode && exitCode != expectedExitCode )
+      {
+        throw new IllegalStateException( "Unexpected exit code for command " + builder.command() + ". " +
+                                         "Actual: " + exitCode + " Expected: " + expectedExitCode );
+      }
+      return exitCode;
+    }
+    catch ( final IOException ioe )
+    {
+      throw new IllegalStateException( "Error starting command " + builder.command(), ioe );
+    }
+    catch ( final InterruptedException ie )
+    {
+      throw new IllegalStateException( "Error waiting for command " + builder.command(), ie );
+    }
+  }
+
+  private static void copy( @Nonnull final InputStream input, @Nonnull final OutputStream output )
+  {
+    try
+    {
+      // Java9 can use input.transferTo(output)
+      try ( final InputStream in = input; final OutputStream out = output )
+      {
+        final byte[] buffer = new byte[ BUFFER_SIZE ];
+        int bytesRead = in.read( buffer );
+        while ( -1 != bytesRead )
+        {
+          out.write( buffer, 0, bytesRead );
+          bytesRead = in.read( buffer );
+        }
+      }
+    }
+    catch ( final IOException ioe )
+    {
+      throw new IllegalStateException( ioe );
+    }
+  }
+}
