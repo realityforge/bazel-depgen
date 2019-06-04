ea6eb78ba1b8e33984a18558f08c50a5eb6af2da
Peter Donald <peter@realityforge.org>
2019-06-04 23:56:54 +1000

    Remove unused field

--- src/main/java/org/realityforge/bazel/depgen/config/Language.java
+++ src/main/java/org/realityforge/bazel/depgen/config/Language.java
@@ -1,23 +1,7 @@
 package org.realityforge.bazel.depgen.config;
 
-import java.util.Objects;
-import javax.annotation.Nonnull;
-
 public enum Language
 {
-  Java( "-java" ),
-  J2cl( "-j2cl" );
-  @Nonnull
-  private final String _targetSuffix;
-
-  Language( @Nonnull final String targetSuffix )
-  {
-    _targetSuffix = Objects.requireNonNull( targetSuffix );
-  }
-
-  @Nonnull
-  public String getTargetSuffix()
-  {
-    return _targetSuffix;
-  }
+  Java,
+  J2cl
 }
