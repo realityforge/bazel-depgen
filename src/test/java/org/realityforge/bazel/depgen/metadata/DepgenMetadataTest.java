de92e4fe5cd5a731a16d94865768f4211d4cba4c
Peter Donald <peter@realityforge.org>
2021-04-15 23:26:39 +1000

    Mark class as final

--- src/test/java/org/realityforge/bazel/depgen/metadata/DepgenMetadataTest.java
+++ src/test/java/org/realityforge/bazel/depgen/metadata/DepgenMetadataTest.java
@@ -20,7 +20,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
-public class DepgenMetadataTest
+public final class DepgenMetadataTest
   extends AbstractTest
 {
   @Test
