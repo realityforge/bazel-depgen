f3202a461b9e66e627745dfa8faec5d545550629
Peter Donald <peter@realityforge.org>
2022-05-25 09:43:44 +1000

    Optimise imports

--- src/main/java/org/realityforge/bazel/depgen/util/StarlarkOutput.java
+++ src/main/java/org/realityforge/bazel/depgen/util/StarlarkOutput.java
@@ -1,7 +1,5 @@
 package org.realityforge.bazel.depgen.util;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
