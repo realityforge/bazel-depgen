c95a84fc66d7bd0a7509c168f28bbff79aeba09c
Peter Donald <peter@realityforge.org>
2023-01-30 16:22:43 +1100

    Add nullability annotations

--- src/main/java/org/realityforge/bazel/depgen/util/OrderedProperties.java
+++ src/main/java/org/realityforge/bazel/depgen/util/OrderedProperties.java
@@ -5,6 +5,7 @@ import java.util.Enumeration;
 import java.util.Properties;
 import java.util.Set;
 import java.util.TreeSet;
+import javax.annotation.Nonnull;
 
 /**
  * A simple customization of Properties that has a stable output order based on alphabetic ordering of keys.
@@ -13,12 +14,14 @@ public final class OrderedProperties
   extends Properties
 {
   @Override
+  @Nonnull
   public synchronized Enumeration<Object> keys()
   {
     return Collections.enumeration( keySet() );
   }
 
   @Override
+  @Nonnull
   public Set<Object> keySet()
   {
     return new TreeSet<>( super.keySet() );
