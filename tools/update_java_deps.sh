9bdbe92fbd0ec35e9bb3438fa89cb6e2b8f419a1
Peter Donald <peter@realityforge.org>
2026-07-02 22:32:09 +1000

    Make sure deps are actually updated

--- tools/update_java_deps.sh
+++ tools/update_java_deps.sh
@@ -33,4 +33,5 @@ java -jar "${JAR}" \
   --config-file tools/java-format/dependencies.yml \
   --cache-directory "${CACHE_DIR}" \
   generate
+bazel run //third_party/java:update_depgen_generated_outputs
 bazel run //:buildifier -- MODULE.bazel third_party/java/BUILD.bazel tools/java-format/BUILD.bazel
